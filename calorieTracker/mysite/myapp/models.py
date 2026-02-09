104b4d37d3c68f222e2d7cc8390c52e315ff61d4
ThatoMusa <thato.m@sactrucks.co.za>
2026-02-09 15:35:03 +0200

    listing consumed food items

--- calorieTracker/mysite/myapp/models.py
+++ calorieTracker/mysite/myapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class food(models.Model):
+    
+    def __str__(self):
+        return self.name
+    
+    name        = models.CharField(max_length=100)
+    carbs       = models.FloatField()
+    protein     = models.FloatField()
+    fat         = models.FloatField()
+    calories    = models.IntegerField()
+    
+    
+class Consume(models.Model):
+    
+    def __str__(self):
+        return self.food.name
+    
+    food        = models.ForeignKey(food, on_delete=models.CASCADE)
+    user        = models.ForeignKey(User, on_delete=models.CASCADE)
--- calorieTracker/mysite/myapp/views.py
+++ calorieTracker/mysite/myapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Consume, food
 
-# Create your views here.
+def index(request):
+    
+    if request.method == 'POST':
+        food_consumed   = request.POST.get('food_consumed')
+        consume         = food.objects.get(name=food_consumed)    
+        user            = request.user
+        consume         = Consume(food=consume, user=user)
+        consume.save()
+        foods           = food.objects.all()
+        consumed_food   = Consume.objects.filter(user=user)
+        
+    else:
+        consumed_food = Consume.objects.filter(user=request.user)
+        foods = food.objects.all()
+    
+    context = {
+        'foods': foods,
+        'consumes': consumed_food
+    }
+    
+    return render(request, 'index.html', context)
